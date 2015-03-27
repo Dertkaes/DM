b048bc9924e96ef790d904a29c1214c48d9c116c
dertkaes <dertkaes@gmail.com>
2015-03-27 13:03:27 +0300

    main form

--- DM/Form1.Designer.cs
+++ DM/Form1.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.диетаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.постотретьДиетыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьДиетыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.раскладкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.посмотретьРакладкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьРаскладкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.диетаToolStripMenuItem,
+            this.раскладкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(254, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // диетаToolStripMenuItem
+            // 
+            this.диетаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьДиетыToolStripMenuItem,
+            this.постотретьДиетыToolStripMenuItem});
+            this.диетаToolStripMenuItem.Name = "диетаToolStripMenuItem";
+            this.диетаToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.диетаToolStripMenuItem.Text = "Диета";
+            // 
+            // постотретьДиетыToolStripMenuItem
+            // 
+            this.постотретьДиетыToolStripMenuItem.Name = "постотретьДиетыToolStripMenuItem";
+            this.постотретьДиетыToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.постотретьДиетыToolStripMenuItem.Text = "Постотреть диеты";
+            // 
+            // добавитьДиетыToolStripMenuItem
+            // 
+            this.добавитьДиетыToolStripMenuItem.Name = "добавитьДиетыToolStripMenuItem";
+            this.добавитьДиетыToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.добавитьДиетыToolStripMenuItem.Text = "Добавить диеты";
+            // 
+            // раскладкаToolStripMenuItem
+            // 
+            this.раскладкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьРаскладкуToolStripMenuItem,
+            this.посмотретьРакладкиToolStripMenuItem});
+            this.раскладкаToolStripMenuItem.Name = "раскладкаToolStripMenuItem";
+            this.раскладкаToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.раскладкаToolStripMenuItem.Text = "Блюда";
+            // 
+            // посмотретьРакладкиToolStripMenuItem
+            // 
+            this.посмотретьРакладкиToolStripMenuItem.Name = "посмотретьРакладкиToolStripMenuItem";
+            this.посмотретьРакладкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.посмотретьРакладкиToolStripMenuItem.Text = "Посмотреть блюда";
+            // 
+            // добавитьРаскладкуToolStripMenuItem
+            // 
+            this.добавитьРаскладкуToolStripMenuItem.Name = "добавитьРаскладкуToolStripMenuItem";
+            this.добавитьРаскладкуToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.добавитьРаскладкуToolStripMenuItem.Text = "Добавить блюдо";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Раскладка на день";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Составить раскладку";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(87, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Шаблоны";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(87, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(40, 87);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(32, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "на ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "дней";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 238);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem диетаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьДиетыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem постотретьДиетыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem раскладкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьРаскладкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem посмотретьРакладкиToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
     }
 }
 
